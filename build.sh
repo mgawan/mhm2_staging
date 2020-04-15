de65fd9a35f28b074e4d3d27e4b8cc772783542c
Steven Hofmeyr <shofmeyr@lbl.gov>
2020-04-14 22:38:01 -0700

    retain previous binary installs

--- build.sh
+++ build.sh
@@ -14,12 +14,13 @@ INSTALL_PATH=${MHMXX_INSTALL_PATH:=$rootdir/install}
 
 if [ "$1" == "clean" ]; then
     rm -rf build/*
-    rm -rf $INSTALL_PATH
+    # if this isn't removed then the the rebuild will not work
+    rm -rf $INSTALL_PATH/cmake
 else
     cd $rootdir/build
     if [ "$1" == "Debug" ] || [ "$1" == "Release" ]; then
         rm -rf build/*
-        rm -rf $INSTALL_PATH
+        rm -rf $INSTALL_PATH/cmake
         cmake $rootdir -DCMAKE_BUILD_TYPE=$1 -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH 
     fi
     make -j install
