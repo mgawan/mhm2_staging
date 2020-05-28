66a4bc26f81af68cf12f08d7df3863c424676bab
Steven Hofmeyr <shofmeyr@lbl.gov>
2020-05-28 14:06:15 -0700

    better headers is script

--- misc-scripts/found-genomes-mq.sh
+++ misc-scripts/found-genomes-mq.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 r=$1
-echo "bin name refdepth genfrac"
-for f in `ls mq-*/summary/TXT/Genome_fraction.txt`; do
+echo "bin1 name refdepth genfrac"
+for b in `seq 0 10000`; do
+    f=mq-bin_$b/summary/TXT/Genome_fraction.txt
+    if [ ! -f "$f" ]; then
+        continue
+    fi
     x=`grep -v "-" $f`
     bin=`echo $x|cut -d' ' -f2|cut -d '_' -f2`
-    echo -n "$bin "
+    echo -n "$bin"
     for i in `echo $x|cut -d' ' -f3,4`; do
         y=`grep $i $r|lastcol.py`
-        echo -n "$i $y"
+        echo -n " $i $y"
     done
     echo ""
 done
