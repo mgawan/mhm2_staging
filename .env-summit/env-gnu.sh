7b1070a6d6bcaeb6206f866a1f9bf870a82affa0
Rob Egan <RSEgan@lbl.gov>
2020-06-11 18:45:08 -0400

    environment for summit build

---
+++ .env-summit/env-gnu.sh
@@ -0,0 +1,11 @@
+module use /gpfs/alpine/world-shared/csc296/summit/modulefiles
+
+
+module rm xl
+module load gcc/9.1.0
+module load git/2.20.1
+module load cmake/3.15.2
+
+module load upcxx/2020.3.0
+
+# cmake -DCMAKE_C_COMPILER=$(which mpicc) -DCMAKE_CXX_COMPILER=$(which mpicxx) 
