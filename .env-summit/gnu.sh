1f28fdb36ae184d2f04662ff460d9adb2a25a3b4
Rob Egan <RSEgan@lbl.gov>
2020-06-18 23:07:40 -0400

    updated cmake

--- .env-summit/gnu.sh
+++ .env-summit/gnu.sh
@@ -4,7 +4,7 @@ module use /gpfs/alpine/world-shared/csc296/summit/modulefiles
 module rm xl
 module load gcc/9.1.0
 module load git/2.20.1
-module load cmake/3.15.2
+module load cmake/3.17.3
 
 module load upcxx/2020.3.0
 
