9ce0b7280a296c2410c8cfb5f032b1f49b52f66f
Steven Hofmeyr <shofmeyr@lbl.gov>
2020-07-02 17:07:13 -0700

    better message for oom failure. reduce alignment ctg fetch buf size

--- src/mhmxx.py
+++ src/mhmxx.py
@@ -342,6 +342,8 @@ def main():
                 signame = ''
                 if -_proc.returncode <= len(SIGNAMES) and _proc.returncode < 0:
                     signame = ' (' + SIGNAMES[-_proc.returncode - 1] + ')'
+                elif _proc.returncode == 137:
+                    signame = ' (OOM)'
                 print_red("\nERROR: subprocess terminated with return code ", -_proc.returncode, signame)
                 err_msgs.append("ERROR: subprocess terminated with return code " + str(-_proc.returncode) + signame)
                 print_err_msgs(err_msgs)
