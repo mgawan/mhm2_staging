c3ac53d46f54d2054574dd5552111475088ad47f
Rob Egan <rsegan@lbl.gov>
2021-02-01 18:27:45 -0800

    modifed the build for cori gpu support

--- contrib/environments/cori-gpu/gnu.sh
+++ contrib/environments/cori-gpu/gnu.sh
@@ -9,18 +9,18 @@ module rm craype-mic-knl
 module rm craype-haswell
 module rm craype-x86-skylake
 
-module load esslurm
+module load cgpu
 
 module load PrgEnv-gnu
 module load craype
 module load craype-x86-skylake
 
 module load cuda
-module load cmake
+module load cmake/3.18.2
 module load git
 module load upcxx-gpu
 
 module list
 
 export OMP_NUM_THREADS=1
-export MHM2_CMAKE_EXTRAS="-DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc"
+export MHM2_CMAKE_EXTRAS="-DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CUDA_ARCHITECTURES='70'"
