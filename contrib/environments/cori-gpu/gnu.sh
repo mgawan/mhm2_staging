1a9c85fc3b1a7da87f76f81367946feac7116983
Rob Egan <rsegan@lbl.gov>
2021-02-02 17:47:00 -0800

    updated gpu build

--- contrib/environments/cori-gpu/gnu.sh
+++ contrib/environments/cori-gpu/gnu.sh
@@ -24,3 +24,6 @@ module list
 
 export OMP_NUM_THREADS=1
 export MHM2_CMAKE_EXTRAS="-DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CUDA_ARCHITECTURES='70'"
+
+# example execute on 2 nodes, with 8 GPUs per node
+# sbatch -C gpu -t 30  --exclusive -A m342 -G 16 -N 2 --wrap="./mhm2-builds/FixCoriGPUBuild/build-gpu-gnu-RelWithDebug/install/bin/mhm2.py -r arctic_sample_0.fq  -v"
